4260061ea38d60bc711c34d2ac9e57d1d9fe5e60
Jasper McCulloch <mccullochjasper@gmail.com>
2022-01-22 19:47:05 +0000

    Include manifest v3 build Remove unnecessary storage permission

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,32 @@
 import { nodeResolve } from "@rollup/plugin-node-resolve";
-import copy from 'rollup-plugin-copy'
+import copy from 'rollup-plugin-copy';
 
-export default {
-  input: `build/index.js`,
-  output: {
-    file: `dist/addon.js`,
-    format: 'iife',
-  },
-  plugins: [
-    nodeResolve(),
-    copy({
-      targets: [
-        {
-          src: 'static/**/*',
-          dest: 'dist'
-        },
-      ]
-    })
-  ],
-};
+const createConfigWithManifest = (manifestVersion) => {
+  const distRoot = `dist/manifest-v${manifestVersion}`;
+
+  return {
+    input: `build/index.js`,
+    output: {
+      file: `${distRoot}/addon.js`,
+      format: 'iife',
+    },
+    plugins: [
+      nodeResolve(),
+      copy({
+        targets: [
+          {
+            src: ['static/*.png', 'static/*.css', 'static/*.js'],
+            dest: distRoot
+          },
+          {
+            src: [`static/manifests/v${manifestVersion}.json`],
+            dest: distRoot,
+            rename: 'manifest.json'
+          },
+        ]
+      })
+    ],
+  };
+}
+
+export default [2, 3].map(createConfigWithManifest);
